0f80bd55d3be22ef51c90f8978ca82d2caec9b06
MaksTymof <maksym198858@gmail.com>
2021-03-29 00:35:23 +0300

    First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SelfRepresentation msg="Welcome to My Portfolio"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import SelfRepresentation from '@/components/SelfRepresentation.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SelfRepresentation
   }
 }
 </script>
